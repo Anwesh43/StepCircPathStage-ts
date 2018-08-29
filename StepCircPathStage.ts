cccc7fba05b1aa02afa3f50b9d2ca192936c4f67
Anwesh43 <anweshthecool0@gmail.com>
2018-08-29 11:13:04 +0530

    adding SCPNode class

--- StepCircPathStage.ts
+++ StepCircPathStage.ts
@@ -1,4 +1,37 @@
 const w : number = window.innerWidth, h : number = window.innerHeight
+const nodes : number = 5
+interface Point {
+    x : number
+    y : number
+}
+function getGap() : number {
+    return (w / (nodes + 1))
+}
+
+function getXY(i : number) : Point {
+    return {x: (i + 1) * getGap(), y : h - ((nodes + 1) * getGap()) + (i + 1) *  getGap()}
+}
+
+function drawQuaCi(context, sc1 : number, sc2 : number) {
+    const gap : number = getGap()
+    var j : number = 0
+    var degStart : number = -90 + 90 * sc2, degEnd : number = -90 + 90 * sc1
+    context.lineWidth = Math.min(w, h) / 60
+    context.lineCap = 'round'
+    context.beginPath()
+    for (var a = degStart; a <= degEnd; a++) {
+        const x : number = gap * Math.cos(a * Math.PI/180)
+        const y : number = gap * Math.sin(a * Math.PI/180)
+        if (j == 0) {
+            context.moveTo(x, y)
+        } else {
+            context.lineTo(x, y)
+        }
+        j++
+    }
+    context.stroke()
+}
+
 class StepCircPathStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
@@ -64,5 +97,57 @@ class Animator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class SCPNode {
+    state : State = new State()
+    prev : SCPNode
+    next : SCPNode
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new SCPNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        context.fillStyle = '#FF8F00'
+        context.strokeStyle = '#FF8F00'
+        const gap : number = getGap()
+        const xy : Point = getXY(this.i)
+        const y : number = xy.y
+        const x : number = xy.x
+        const sc1 : number = Math.min(0.5, this.state.scale) * 2
+        const sc2 : number = Math.min(0.5, Math.max(this.state.scale - 0.5, 0)) * 2
+        context.save()
+        context.translate(x, y)
+        drawQuaCi(context, sc1, sc2)
+        context.fillRect(-gap, -gap, gap, gap)
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SCPNode {
+        var curr : SCPNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
