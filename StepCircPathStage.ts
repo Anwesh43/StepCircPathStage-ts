e7d37c4f811773eb3f92fc801966f32e3f30102b
Anwesh43 <anweshthecool0@gmail.com>
2018-08-29 10:29:54 +0530

    adding State class

--- StepCircPathStage.ts
+++ StepCircPathStage.ts
@@ -24,3 +24,26 @@ class StepCircPathStage {
         }
     }
 }
+
+class State {
+    scale = 0
+    dir = 0
+    prevScale = 0
+
+    update(cb : Function) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
