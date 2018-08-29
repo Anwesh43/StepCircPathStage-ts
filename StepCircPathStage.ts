cb85a039be0aad028a66d44916534ce59d8497a5
Anwesh43 <anweshthecool0@gmail.com>
2018-08-29 11:15:06 +0530

    adding StepCircPath class

--- StepCircPathStage.ts
+++ StepCircPathStage.ts
@@ -151,3 +151,24 @@ class SCPNode {
         return this
     }
 }
+
+class StepCircPath {
+    curr : SCPNode = new SCPNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
